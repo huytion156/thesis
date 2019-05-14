75d168cda68bb0cdfc7b4e69d61ba5d8ac4b527e
Huy Huynh <huytion156@gmail.com>
2019-05-14 15:10:13 +0700

    Initial Commit

---
+++ thesis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  thesis
+//
+//  Created by Huynh Vo Nhat Huy on 5/14/19.
+//  Copyright © 2019 15CNTN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
